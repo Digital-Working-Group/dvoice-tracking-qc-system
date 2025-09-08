b8c310c0e65f3ef6e1513d4154dcef882e203a5c
Cody Karjadi <ckarjadi@fhs.org>
2025-09-08 16:51:52 -0400

    add docker support

--- requirements/python3-9-6/run_docker.sh
+++ requirements/python3-9-6/run_docker.sh
@@ -4,14 +4,14 @@ if [ $# -eq 1 ];
 then
     container_name=$1
 else
-    container_name="qc-system-py-3-13-1-ctr"
+    container_name="qc-system-py-3-9-6-ctr"
 fi
 
 if [ $# -eq 2 ];
 then
     docker_name=$1
 else
-    docker_name="qc-system-py-3-13-1"
+    docker_name="qc-system-py-3-9-6"
 fi
 dir_up=$(realpath "../../")
 docker run -v $dir_up:/scripts/ -it --rm --name $container_name $docker_name bash
